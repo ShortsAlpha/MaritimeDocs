94b1fbcd3a9d9c8e0f8d5c64db70c550813dfbd3
ShortsAlpha <yadaumur127@gmail.com>
2026-04-20 16:51:37 +0300

    Fix: Remove temporary debug scripts to resolve Vercel build type errors

--- scratch_parse.ts
+++
@@ -1,9 +0,0 @@
-import { read, utils } from 'xlsx';
-import fs from 'fs';
-
-const buf = fs.readFileSync('public/templates/MALTA - Course Flow - MOY Limited (1).xlsx');
-const wb = read(buf, { type: 'buffer' });
-const ws = wb.Sheets[wb.SheetNames[0]];
-const data = utils.sheet_to_json(ws, { header: 1 });
-
-console.log(JSON.stringify(data.slice(0, 40), null, 2));
